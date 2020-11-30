c867728cd21dfe53af91aa3ab1fc4b7cb3d920c1
kanari3 <kanari333333@gmail.com>
2020-11-30 23:38:28 +0900

    setup backgroud notify

--- android/app/src/main/kotlin/jp/andpad/android/flutter/pushnotification/MainActivity.kt
+++ android/app/src/main/kotlin/jp/andpad/android/flutter/pushnotification/MainActivity.kt
@@ -3,4 +3,11 @@ package jp.andpad.android.flutter.pushnotification
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+    }
+
+
 }
